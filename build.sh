9c8269fef5a0f7c4ee2503500beeb69063493db4
xafero <xafero@users.noreply.github.com>
2025-04-13 15:21:07 +0200

    Adding win build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained false src/XMLCmd -o pub
+dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained false src/XMLCmd -o pub
 

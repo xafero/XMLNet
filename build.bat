ce6599ca81b95ae79566bec1b0ae5bd34f119695
xafero <xafero@users.noreply.github.com>
2025-04-15 18:09:52 +0200

    Adding script

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained false src\XMLCmd -o pub
+dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained false src\XMLCmd -o pub
+
